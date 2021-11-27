036bb95badf46328736fe8546b15104c6ecf1db2
unknown <krushnatkhawale@gmail.com>
2021-11-28 00:04:31 +0530

    Card view and fields updated

--- tailwind/js/index.js
+++ tailwind/js/index.js
@@ -3,55 +3,34 @@ var card = '<div class="min-h-screen bg-gray-100 flex items-center justify-cente
                 '<div class="p-4">'+
                 '    <img class="rounded-xl" src="{{img_src}}" alt="Dog" />'+
                 '</div>'+
-                '<div class="flex justify-between p-6">'+
-                '    <div class="flex items-center space-x-4">'+
-                '    <img class="h-10 rounded-full" src="https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="" />'+
-                '    <h1 class="text-lg text-gray-900 font-bold">Felipe Sacudon</h1>'+
-                '    </div>'+
-                '    <div class="flex space-x-6 items-center">'+
-                '    <div class="flex space-x-2 items-center">'+
-                '        <span>'+
-                '        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">'+
-                '            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />'+
-                '        </svg>'+
-                '        </span>'+
-                '        <span class="text-gray-700 font-semibold">20</span>'+
-                '    </div>'+
-                '    <div class="flex space-x-2 items-center pr-4">'+
-                '        <span>'+
-                '        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-600 hover:text-red-500" viewBox="0 0 20 20" fill="currentColor">'+
-                '            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />'+
-                '        </svg>'+
-                '        </span>'+
-                '        <span class="text-gray-700 font-semibold">22</span>'+
-                '    </div>'+
-                '    </div>'+
+                '<div class="p-6">'+
+                '    <h3 class="text-lg text-gray-900">Camera: {{cam_name}}</h3>'+
+                '    <h3 class="text-lg text-gray-900" title="Day since rover landed on Mars">Mars day: {{mars_day}}</h3>'+
                 '</div>'+
                 '</div>'+
             '</div>';
 
-var isToday = (someDate) => {
+var isTodayOrYesterday = (someDate) => {
   someDate = new Date(someDate)
   var today = new Date()
-  return someDate.getDate() == today.getDate() &&
+  return (someDate.getDate() == today.getDate() || someDate.getDate()==today.getDate()-1)&&
     someDate.getMonth() == today.getMonth() &&
     someDate.getFullYear() == today.getFullYear()
 }
+
 
 function fetchPhotos(){
 
     var requestedDateField = document.getElementById('requestedDate');
     var requestedDate = requestedDateField.value;
-
-    console.log(requestedDate);
-
     var bodyHeaderDiv = document.getElementById('bodyHeader');
 
-    if(isToday(requestedDate)){
+    if(isTodayOrYesterday(requestedDate)){
+        console.log("Can't retrieve for requested date: ", requestedDate);
         bodyHeaderDiv.innerHTML = "Usually, NASA api's most recent response is for two earth days in past, please select a date earlier than last two days";
+        bodyHeaderDiv.classList.remove('scale-0');
         return;
     }
-
 
     var nasaApiUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+ requestedDate +"&api_key=DEMO_KEY";
     var xhttp = new XMLHttpRequest();
@@ -62,10 +41,14 @@ function fetchPhotos(){
 
           console.log("Response received successfully");
           console.log(data);
+          bodyHeaderDiv.classList.add('scale-0');
+          //Todo: getStats(data);
           var noOfPhotos = data['photos'].length;
           if(noOfPhotos){
             for(var i=0;i<noOfPhotos;i++){
-                var aCard = card.replace("{{img_src}}", data['photos'][i]['img_src']);
+                var aCard = card.replace("{{img_src}}", data['photos'][i]['img_src'])
+                                .replace("{{cam_name}}", data['photos'][i]['camera']['name'])
+                                .replace("{{mars_day}}", data['photos'][i]['sol']);
                 var parentDiv = document.getElementById('gallery-container');
                 var div = document.createElement('div');
                 div.innerHTML = aCard;
@@ -74,12 +57,15 @@ function fetchPhotos(){
             }
           } else {
             console.log("No photos received");
+
             bodyHeaderDiv.innerHTML = "Sorry, No photos found for requested date: " + requestedDate;
+            bodyHeaderDiv.classList.remove('scale-0');
           }
           console.log("Completed");
         } else {
             console.log("Http issue while talking to NASA api, status is ", this.status)
             bodyHeaderDiv.innerHTML = "Apologies, Free api call limit reached, NASA api with public key only provides 500 requests a day, please try tomorrow, sorry for inconvenience";
+            bodyHeaderDiv.classList.remove('scale-0');
        }
     };
     xhttp.open("GET", nasaApiUrl, true);
