6af8f7355b0167a284ec27e25e540958714bc6ae
krushnat <krushnatkhawale@gmail.com>
2022-12-20 01:40:15 +0530

    mars explorer v2: layout enhancements

--- projects/mars-explorer/js/index2.js
+++ projects/mars-explorer/js/index2.js
@@ -1,11 +1,4 @@
-var card1 = '<div class="rounded overflow-hidden shadow-lg">' +
-           '    <img class="w-96" src="{{img_src}}" alt="Mountain">' +
-           '    <div class="px-6 pt-4 pb-2">' +
-           '      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>' +
-           '      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>' +
-           '   </div>' +
-           '</div>';
-var card = '<div class="col"><div class="card" style="width: 18rem; height: 100%;">'+
+var card = '<div class="col"><div class="card" style="width: 18rem; height: 225px;">'+
            '  <img alt="gallery" style="object-fit: cover; height: 100%;" class="" src="{{img_src}}">'+
            '  <div class="card-body">'+
            '  </div>'+
