9cc7b575764eb0e679776aff6e8d72886d870a1b
Krushnat <krushnatkhawale@gmail.com>
2025-05-24 00:47:15 +0530

    Contact links updated with logo and formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css'; // You can create this file for styling
+import { FaLinkedin, FaGithub, FaEnvelope, FaTwitter, FaStackOverflow } from 'react-icons/fa'; // Import desired icons
 
 function App() {
   return (
@@ -29,10 +30,41 @@ function App() {
         </section>
         <section>
           <h2>Contact</h2>
-          <p>
-            How can people get in touch with you?
-            (e.g., LinkedIn, GitHub, Email)
-          </p>
+          <div className="contact-links">
+            <p>You can reach out to me via:</p>
+            <ul>
+              <li>
+                <a href="https://github.com/krushnatkhawale" target="_blank" rel="noopener noreferrer">
+                  <FaGithub className="contact-icon" />
+                  <span>GitHub</span>
+                </a>
+              </li>
+              <li>
+                <a href="mailto:krushnatkhawale@gmail.com">
+                  <FaEnvelope className="contact-icon" />
+                  <span>Email</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/krushnatkhavale/" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin className="contact-icon" />
+                  <span>LinkedIn</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://stackoverflow.com/users/1446358/krushnat-khavale" target="_blank" rel="noopener noreferrer">
+                  <FaStackOverflow className="contact-icon" />
+                  <span> X </span>
+                </a>
+              </li>
+              <li>
+                <a href="https://x.com/krushnatkhavale" target="_blank" rel="noopener noreferrer">
+                  <FaTwitter className="contact-icon" />
+                  <span> X </span>
+                </a>
+              </li>
+             </ul>
+          </div>
         </section>
       </main>
       <footer>
